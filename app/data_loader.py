0c6726abf8c1eafa12994db597b4e403a7b86540
VaibhavBiyawala <vb.vaibhavbiyawala08@gmail.com>
2024-12-10 18:57:22 +0530

    folder path

--- app/data_loader.py
+++ app/data_loader.py
@@ -3,10 +3,10 @@ import os
 
 def load_files(file1, file2, file3, file4):
     
-    file1 = pd.read_csv(os.path.join('upload', file1))
-    file2 = pd.read_excel(os.path.join('upload', file2))
-    file3 = pd.read_excel(os.path.join('upload', file3))
-    file4 = pd.read_excel(os.path.join('upload', file4))
+    file1 = pd.read_csv(os.path.join(file1))
+    file2 = pd.read_excel(os.path.join(file2))
+    file3 = pd.read_excel(os.path.join(file3))
+    file4 = pd.read_excel(os.path.join(file4))
 
     # Ensure consistent column names
     file1.rename(columns={'TRF ID': 'trf_id'}, inplace=True)
